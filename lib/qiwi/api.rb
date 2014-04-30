4a228e1ebcc0b65809e37e4c6e048a7fe7e01d63
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-04-30 07:38:54 -0700

    Update to latest generated code
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/qiwi/api.rb
+++ lib/qiwi/api.rb
@@ -14,90 +14,118 @@ module Killbill #:nodoc:
               ::Killbill::Qiwi::QiwiResponse)
       end
 
-      def authorize_payment(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, context)
+      def authorize_payment(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, properties, context)
         # Pass extra parameters for the gateway here
         options = {}
-        super(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, context, options)
+
+        properties = merge_properties(properties, options)
+        super(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, properties, context)
       end
 
-      def capture_payment(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, context)
+      def capture_payment(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, properties, context)
         # Pass extra parameters for the gateway here
         options = {}
-        super(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, context, options)
+
+        properties = merge_properties(properties, options)
+        super(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, properties, context)
       end
 
-      def void_payment(kb_account_id, kb_payment_id, kb_payment_method_id, context)
+      def void_payment(kb_account_id, kb_payment_id, kb_payment_method_id, properties, context)
         # Pass extra parameters for the gateway here
         options = {}
-        super(kb_account_id, kb_payment_id, kb_payment_method_id, context, options)
+
+        properties = merge_properties(properties, options)
+        super(kb_account_id, kb_payment_id, kb_payment_method_id, properties, context)
       end
 
-      def process_payment(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, context)
+      def process_payment(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, properties, context)
         # Pass extra parameters for the gateway here
         options = {}
-        super(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, context, options)
+
+        properties = merge_properties(properties, options)
+        super(kb_account_id, kb_payment_id, kb_payment_method_id, amount, currency, properties, context)
       end
 
-      def get_payment_info(kb_account_id, kb_payment_id, context)
+      def get_payment_info(kb_account_id, kb_payment_id, properties, context)
         super
       end
 
-      def search_payments(search_key, offset, limit, context)
+      def search_payments(search_key, offset, limit, properties, context)
         super
       end
 
-      def process_refund(kb_account_id, kb_payment_id, refund_amount, currency, context)
+      def process_refund(kb_account_id, kb_payment_id, refund_amount, currency, properties, context)
         # Pass extra parameters for the gateway here
         options = {}
-        super(kb_account_id, kb_payment_id, refund_amount, currency, context, options)
+
+        properties = merge_properties(properties, options)
+        super(kb_account_id, kb_payment_id, refund_amount, currency, properties, context)
       end
 
-      def get_refund_info(kb_account_id, kb_payment_id, context)
+      def get_refund_info(kb_account_id, kb_payment_id, properties, context)
         super
       end
 
-      def search_refunds(search_key, offset, limit, context)
+      def search_refunds(search_key, offset, limit, properties, context)
         super
       end
 
-      def add_payment_method(kb_account_id, kb_payment_method_id, payment_method_props, set_default, context)
+      def add_payment_method(kb_account_id, kb_payment_method_id, payment_method_props, set_default, properties, context)
         # Pass extra parameters for the gateway here
         options = {}
-        super(kb_account_id, kb_payment_method_id, payment_method_props, set_default, context, options)
+
+        properties = merge_properties(properties, options)
+        super(kb_account_id, kb_payment_method_id, payment_method_props, set_default, properties, context)
       end
 
-      def delete_payment_method(kb_account_id, kb_payment_method_id, context)
+      def delete_payment_method(kb_account_id, kb_payment_method_id, properties, context)
         # Pass extra parameters for the gateway here
         options = {}
-        super(kb_account_id, kb_payment_method_id, context, options)
+
+        properties = merge_properties(properties, options)
+        super(kb_account_id, kb_payment_method_id, properties, context)
       end
 
-      def get_payment_method_detail(kb_account_id, kb_payment_method_id, context)
+      def get_payment_method_detail(kb_account_id, kb_payment_method_id, properties, context)
         super
       end
 
-      def set_default_payment_method(kb_account_id, kb_payment_method_id, context)
+      def set_default_payment_method(kb_account_id, kb_payment_method_id, properties, context)
         # TODO
       end
 
-      def get_payment_methods(kb_account_id, refresh_from_gateway, context)
+      def get_payment_methods(kb_account_id, refresh_from_gateway, properties, context)
         super
       end
 
-      def search_payment_methods(search_key, offset, limit, context)
+      def search_payment_methods(search_key, offset, limit, properties, context)
         super
       end
 
-      def reset_payment_methods(kb_account_id, payment_methods)
+      def reset_payment_methods(kb_account_id, payment_methods, properties, context)
         super
       end
 
-      def build_form_descriptor(kb_account_id, descriptor_fields, context)
-        super
+      def build_form_descriptor(kb_account_id, descriptor_fields, properties, context)
+        # Pass extra parameters for the gateway here
+        options = {}
+        properties = merge_properties(properties, options)
+
+        # Add your custom static hidden tags here
+        options = {
+            #:token => config[:qiwi][:token]
+        }
+        descriptor_fields = merge_properties(descriptor_fields, options)
+
+        super(kb_account_id, descriptor_fields, properties, context)
       end
 
-      def process_notification(notification, context)
-        super
+      def process_notification(notification, properties, context)
+        # Pass extra parameters for the gateway here
+        options = {}
+        properties = merge_properties(properties, options)
+
+        super(notification, properties, context)
       end
     end
   end
--- spec/qiwi/base_plugin_spec.rb
+++ spec/qiwi/base_plugin_spec.rb
@@ -14,10 +14,11 @@ describe Killbill::Qiwi::PaymentPlugin do
       eos
       file.close
 
-      @plugin = Killbill::Qiwi::PaymentPlugin.new
-      @plugin.logger = Logger.new(STDOUT)
+      @plugin              = Killbill::Qiwi::PaymentPlugin.new
+      @plugin.logger       = Logger.new(STDOUT)
       @plugin.logger.level = Logger::INFO
-      @plugin.conf_dir = File.dirname(file)
+      @plugin.conf_dir     = File.dirname(file)
+      @plugin.kb_apis      = Killbill::Plugin::KillbillApi.new('qiwi', {})
 
       # Start the plugin here - since the config file will be deleted
       @plugin.start_plugin
@@ -26,5 +27,66 @@ describe Killbill::Qiwi::PaymentPlugin do
 
   it 'should start and stop correctly' do
     @plugin.stop_plugin
+  end
+
+  it 'should generate forms correctly' do
+    kb_account_id = SecureRandom.uuid
+    kb_tenant_id  = SecureRandom.uuid
+    context       = @plugin.kb_apis.create_context(kb_tenant_id)
+    fields        = @plugin.hash_to_properties({
+                                                   :order_id   => '1234',
+                                                   :account_id => '849384',
+                                                   :amount     => 10
+                                               })
+    form          = @plugin.build_form_descriptor kb_account_id, fields, [], context
+
+    form.kb_account_id.should == kb_account_id
+    form.form_method.should == 'GET'
+    form.form_url.should == 'https://w.qiwi.ru/payments.action'
+
+    form_fields = @plugin.properties_to_hash(form.form_fields)
+    form_fields.size.should == 3
+    form_fields[:id].should == '1234'
+    form_fields[:account].should == '849384'
+    form_fields[:amount].should == '10'
+  end
+
+  it 'should receive notifications correctly' do
+    transaction_id = '9943820274192'
+    transaction_date = '20140427074818'
+    account_id     = 98321
+    response_code  = 0
+    description    = 'description'
+    gross = 500
+
+    kb_tenant_id = SecureRandom.uuid
+    context      = @plugin.kb_apis.create_context(kb_tenant_id)
+    properties   = @plugin.hash_to_properties({ :description => description })
+
+    notification    = "command=check&txn_id=#{transaction_id}&account=#{account_id}"
+    gw_notification = @plugin.process_notification notification, properties, context
+    gw_notification.entity.should == <<-eos
+<?xml version="1.0" encoding="UTF-8"?>
+<response>
+  <osmp_txn_id>#{transaction_id}</osmp_txn_id>
+  <prv_txn>#{account_id}</prv_txn>
+  <sum></sum>
+  <result>#{response_code}</result>
+  <comment>#{description}</comment>
+</response>
+    eos
+
+    notification    = "command=pay&txn_id=#{transaction_id}&txn_date=#{transaction_date}&account=#{account_id}&sum=#{gross}"
+    gw_notification = @plugin.process_notification notification, properties, context
+    gw_notification.entity.should == <<-eos
+<?xml version="1.0" encoding="UTF-8"?>
+<response>
+  <osmp_txn_id>#{transaction_id}</osmp_txn_id>
+  <prv_txn>#{account_id}</prv_txn>
+  <sum>#{gross}</sum>
+  <result>#{response_code}</result>
+  <comment>#{description}</comment>
+</response>
+    eos
   end
 end
